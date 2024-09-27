88583a9c814fea240a766a6ee9da774c541e3ebe
Alexis Sushko <alexis.sushko@gmail.com>
2024-09-26 20:44:03 -0400

    Added link to netlify in comments of App.jsx

--- block-26/src/App.jsx
+++ block-26/src/App.jsx
@@ -1,5 +1,7 @@
 //Worked on project with Joy, Kristen, David, Bertha, and Blake
 
+//https://app.netlify.com/sites/bespoke-cascaron-834b32/deploys/66f5fd653931c38791384724
+
 import { useState } from "react";
 import ContactList from "./Components/ContactList";
 import SelectedContact from "./Components/SelectedContact";
